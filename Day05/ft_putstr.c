9009b00b753ff620f88d3653d4561ade399e5bba
Andrew Minai <andrewminai24@gmail.com>
2019-04-03 19:43:40 -0700

    ft_putstr.c

---
+++ Day05/ft_putstr.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
+
+void ft_putstr(char *str)
+{
+	while(*str != '\0')
+	{
+	ft_putchar(*str);
+	str++;
+	}
+}	
+int main(void)
+{
+	ft_putstr("hello");
+	return(0);
+}
